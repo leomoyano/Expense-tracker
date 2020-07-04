268241c5b58676aa19bf554af79af8dfd4484b5d
unknown <leomoyano7@gmail.com>
2020-07-04 13:32:45 -0300

    default setup

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -4,12 +4,7 @@ import App from '../App';
 
 //Initial State
 const initialState = {
-    transactions: [
-              { id: 1, text: 'Flower', amount: -20 },
-              { id: 2, text: 'Salary', amount: 300 },
-              { id: 3, text: 'Book', amount: -10 },
-              { id: 4, text: 'Camera', amount: 150 }
-    ]
+    transactions: []
 }
 
 
